d25a0345f5526068958a99911c2cff32934556d4
TShaffer133 <tyroneshaffer133@gmail.com>
2019-01-08 04:52:39 -0500

    Made site more mobile-friendly by removing side-bar content from view. Added pictures/placeholders.

--- web-portfolio/src/app/components/about/about.component.ts
+++ web-portfolio/src/app/components/about/about.component.ts
@@ -8,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   code = [
-    { title: 'Perceptron', description: 'About'},
-    { title: 'mARkit', description: 'Portfolio' },
-    { title: 'This Website', description: 'Blog' },
+    { title: 'Logistic Perceptron', subtitle: 'Programmer', image: '../../../assets/perceptronTest.png',
+    description: 'Created a Logistic Regression-based Perceptron that could \
+    categorize data into distinct subgroups e\.g\. legitimate vs spam mail',
+    buzzwords: 'AI, Machine Learning, Java',
+    github: "https://github.com/TShaffer133"},
+    { title: 'mARkit', subtitle: 'Project Lead', description: 'Portfolio',
+    buzzwords: 'Augmented Reality, Full-Stack'},
+    { title: 'This Website', subtitle: 'Creator?', description: 'Blog',
+    buzzwords: 'Full-Stack, Angular, Node\.js, SQL',
+    github: "https://github.com/TShaffer133/FolioCode"},
   ];
 
   constructor() { }
