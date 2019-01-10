c765fad48b16e697793686ee090d1e00dd25b431
TShaffer133 <tyroneshaffer133@gmail.com>
2019-01-10 02:25:05 -0500

    Added more descriptions, adjusted sizing

--- web-portfolio/src/app/components/about/about.component.ts
+++ web-portfolio/src/app/components/about/about.component.ts
@@ -8,14 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   code = [
-    { title: 'Logistic Perceptron', subtitle: 'Programmer', image: '../../../assets/perceptronTest.png',
+    { title: 'Logistic Perceptron',
+    subtitle: 'Programmer',
+    image: '../../../assets/perceptronTest.png',
     description: 'Created a Logistic Regression-based Perceptron that could \
     categorize data into distinct subgroups e\.g\. legitimate vs spam mail',
     buzzwords: 'AI, Machine Learning, Java',
     github: "https://github.com/TShaffer133"},
-    { title: 'mARkit', subtitle: 'Project Lead', description: 'Portfolio',
+
+    { title: 'mARkit',
+    subtitle: 'Project Lead',
+    image: '../../../assets/mARkit.png',
+    description: 'Spearheaded the development of an AR mobile application that \
+    was aimed to provide users with a 3D preview of items before they were purchased',
+    github: "https://github.com/TShaffer133",
     buzzwords: 'Augmented Reality, Full-Stack'},
-    { title: 'This Website', subtitle: 'Creator?', description: 'Blog',
+
+    { title: 'This Website',
+    subtitle: 'Creator?',
+    description: 'Blog',
     buzzwords: 'Full-Stack, Angular, Node\.js, SQL',
     github: "https://github.com/TShaffer133/FolioCode"},
   ];
